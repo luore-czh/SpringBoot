6b80cab1794c76244cb44840aaf190e0ee56ac24
luore-czh <2510709235@qq.com>
2023-05-17 17:34:44 +0800

    SpringBoot学习的第一天

---
+++ src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//这是一个SpringBoot应用
+//主程序类
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ src/main/java/com/atguigu/boot/config/MyConfig.java
@@ -0,0 +1,9 @@
+package com.atguigu.boot.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//告诉Spring Boot这是一个配置类
+@Configuration
+public class MyConfig {
+
+}
